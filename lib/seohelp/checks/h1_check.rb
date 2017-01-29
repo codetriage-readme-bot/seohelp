652c08b2b2a8eb18479e44128dd79b752187478f
Xavi Vila <xavivm@gmail.com>
2017-01-29 21:56:03 +0000

    fix tests

--- lib/seohelp/checks/h1_check.rb
+++ lib/seohelp/checks/h1_check.rb
@@ -7,10 +7,10 @@ module Seohelp
   class H1Check < Seohelp::Check
     def check
       h1 = @doc.css('h1')
-      if !h1
+      if h1.count == 0
         @warnings << 'h1 not found'
       elsif h1.count > 1
-        @warnings << 'more than 1 h1'
+        @warnings << 'more than 1 h1 present'
       elsif h1.first.text.empty? # todo: add blank? ?
         @warnings << 'empty h1'
       end
